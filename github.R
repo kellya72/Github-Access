a5e99f426b16a3c447fe3c1daf8d7896b2b00350
kellya72 <32570386+kellya72@users.noreply.github.com>
2017-11-30 17:43:37 +0000

    Add files via upload

--- github.R
+++ github.R
@@ -4,3 +4,10 @@ library(jsonlite)
 library(httpuv)
 #install.packages("httr")
 library(httr)
+
+oauth_endpoints("github")
+
+# Change based on what you 
+myapp <- oauth_app(appname = "kellya72cs3012",
+                   key = "fef796b7cc0c4b14c92b",
+                   secret = "c3a5fd5b70e7e893593997a73705e9e988d993c0")
