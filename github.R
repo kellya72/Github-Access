a16aecd93dd803979673f4c3ab59c463320382fc
kellya72 <32570386+kellya72@users.noreply.github.com>
2017-12-06 13:45:59 +0000

    Add files via upload

--- github.R
+++ github.R
@@ -11,3 +11,5 @@ oauth_endpoints("github")
 myapp <- oauth_app(appname = "kellya72cs3012",
                    key = "fef796b7cc0c4b14c92b",
                    secret = "c3a5fd5b70e7e893593997a73705e9e988d993c0")
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+?oauth2.0_token()
