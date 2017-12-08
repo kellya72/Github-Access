7ebd3254cdbeed0da6a1d454179445f71fc41b22
kellya72 <32570386+kellya72@users.noreply.github.com>
2017-12-08 11:56:12 +0000

    Add files via upload

--- github.R
+++ github.R
@@ -4,7 +4,7 @@ library(jsonlite)
 library(httpuv)
 #install.packages("httr")
 library(httr)
-
+library(xml2)
 oauth_endpoints("github")
 
 # Change based on what you 
@@ -12,4 +12,24 @@ myapp <- oauth_app(appname = "kellya72cs3012",
                    key = "fef796b7cc0c4b14c92b",
                    secret = "c3a5fd5b70e7e893593997a73705e9e988d993c0")
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
-?oauth2.0_token()
+
+gtoken <- config(token = github_token)
+ 
+getFollowers <- function(username)
+{
+  
+}
+getFollowing <- function(username)
+{
+  
+}
+getCurrentUserFollowers <- function()
+{
+  
+}
+getNumberOfCommits <- function(username){
+  
+}
+getNumberOfRepositories <- function(username){
+  
+}
